92c3c406849fdcee79b8fd9d048874206c36ff1c
Abraham Kapambwe <abrahamkapambwe@gmail.com>
2026-06-16 09:36:59 +0200

    deploy: Banking.Admin.Client @ 2026-06-16 09:36 UTC

--- service-worker.js
+++ service-worker.js
@@ -1,28 +1,32 @@
-/* Manifest version: 46TZC3Du */
-// Production service worker - aggressive caching
+﻿// Development service worker - minimal caching
+self.importScripts('service-worker-assets.js');
 self.addEventListener('install', event => event.waitUntil(onInstall(event)));
 self.addEventListener('activate', event => event.waitUntil(onActivate(event)));
 self.addEventListener('fetch', event => event.respondWith(onFetch(event)));
 
 const cacheNamePrefix = 'offline-cache-';
-const cacheName = `${cacheNamePrefix}${self.assetsManifest?.version ?? "v1"}`;
+const cacheName = `${cacheNamePrefix}${self.assetsManifest?.version ?? 'dev'}`;
 const offlineAssetsInclude = [ /\.dll$/, /\.pdb$/, /\.wasm/, /\.html/, /\.js$/, /\.json$/, /\.css$/, /\.woff$/, /\.png$/, /\.jpe?g$/, /\.gif$/, /\.ico$/, /\.blat$/, /\.dat$/ ];
 const offlineAssetsExclude = [ /^service-worker\.js$/ ];
 
 async function onInstall(event) {
     console.info('Service worker: Install');
     
-    const assetsRequests = self.assetsManifest.assets
-        .filter(asset => offlineAssetsInclude.some(pattern => pattern.test(asset.url)))
-        .filter(asset => !offlineAssetsExclude.some(pattern => pattern.test(asset.url)))
-        .map(asset => new Request(asset.url, { integrity: asset.hash, cache: 'no-cache' }));
+    // In development, we might not have assetsManifest.assets yet
+    if (self.assetsManifest && self.assetsManifest.assets) {
+        const assetsRequests = self.assetsManifest.assets
+            .filter(asset => offlineAssetsInclude.some(pattern => pattern.test(asset.url)))
+            .filter(asset => !offlineAssetsExclude.some(pattern => pattern.test(asset.url)))
+            .map(asset => new Request(asset.url, { integrity: asset.hash, cache: 'no-cache' }));
+        
+        await caches.open(cacheName).then(cache => cache.addAll(assetsRequests));
+    }
     
-    await caches.open(cacheName).then(cache => cache.addAll(assetsRequests));
+    self.skipWaiting();
 }
 
 async function onActivate(event) {
     console.info('Service worker: Activate');
-    
     const cacheKeys = await caches.keys();
     await Promise.all(cacheKeys
         .filter(key => key.startsWith(cacheNamePrefix) && key !== cacheName)
@@ -37,6 +41,5 @@ async function onFetch(event) {
         const cache = await caches.open(cacheName);
         cachedResponse = await cache.match(request);
     }
-    
     return cachedResponse || fetch(event.request);
 }
